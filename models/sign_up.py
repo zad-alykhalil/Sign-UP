9a31c6e572f1714af154a1f040293c3f56b36788
zad-alykhalil <you@example.com>
2023-07-04 18:37:20 +0300

    regiteration form task

--- models/sign_up.py
+++ models/sign_up.py
@@ -1,27 +1,44 @@
-from odoo import api, fields, models
 from odoo import http, tools, _
 from odoo.exceptions import UserError
 from odoo.addons.auth_signup.controllers.main import AuthSignupHome
 from odoo.http import request
 from odoo.addons.web.controllers.home import ensure_db, Home, SIGN_UP_REQUEST_PARAMS, LOGIN_SUCCESSFUL_PARAMS
-from odoo.addons.auth_signup.models.res_users import SignupError
+
 
 SIGN_UP_REQUEST_PARAMS = {'db', 'login', 'debug', 'token', 'message', 'error', 'scope', 'mode',
                           'redirect', 'redirect_hostname', 'email', 'name', 'partner_id',
                           'password', 'confirm_password', 'city', 'country_id', 'lang', 'signup_email', 'phone'}
 
-SIGNED = False
 
+class SignupForm(AuthSignupHome):
 
-class SignHome(Home):
+    def get_auth_signup_qcontext(self):
+        """ Shared helper returning the rendering context for signup and reset password """
+        qcontext = {k: v for (k, v) in request.params.items() if k in SIGN_UP_REQUEST_PARAMS}
+        qcontext.update(self.get_auth_signup_config())
+        if not qcontext.get('token') and request.session.get('auth_signup_token'):
+            qcontext['token'] = request.session.get('auth_signup_token')
+        if qcontext.get('token'):
+            try:
+                # retrieve the user info (name, login or email) corresponding to a signup token
+                token_infos = request.env['res.partner'].sudo().signup_retrieve_info(qcontext.get('token'))
+                for k, v in token_infos.items():
+                    qcontext.setdefault(k, v)
+            except:
+                qcontext['error'] = _("Invalid signup token")
+                qcontext['invalid_token'] = True
+        return qcontext
 
-    def web_login(self, redirect=None, **kw):
-        res = super().web_login()
-        values = {k: v for k, v in request.params.items() if k in SIGN_UP_REQUEST_PARAMS}
-        # print(SIGN_UP_REQUEST_PARAMS)
+    def _prepare_signup_values(self, qcontext):
+        values = {key: qcontext.get(key) for key in ('login', 'name', 'password', 'phone')}
         phone = values.get('phone')
-        # print('________________2 ', phone)
-        # validates the phone number
+
+        print(values)
+        if not values:
+            raise UserError(_("The form was not properly filled in."))
+        if values.get('password') != qcontext.get('confirm_password'):
+            raise UserError(_("Passwords do not match; please retype them."))
+
         if 'phone' in values:
             if len(phone) != 11:
                 raise UserError(_("Phone Number is not valid please retype it."))
@@ -30,24 +47,16 @@ class SignHome(Home):
                 if ord(number) >= 48 and ord(number) <= 57:
                     pass
                 else:
-                    raise UserError("Phone Number is not valid; please retype it.")
+                    raise UserError("Phone Number is not valid please retype it.")
 
             if phone[0] != '0' or phone[1] != '1':
-                raise UserError(_("Phone Number is not valid; please retype it."))
+                raise UserError(_("Phone Number is not valid please retype it."))
 
             if phone[2] != '0' and phone[2] != '1' and phone[2] != '2' and phone[2] != '5':
-                raise UserError(_("Phone Number is not valid; please retype it."))
-            global SIGNED
-            SIGNED = True
+                raise UserError(_("Phone Number is not valid please retype it."))
 
-        return res
-
-
-class SignupForm(AuthSignupHome):
-
-
-    def do_signup(self, qcontext):
-        if not SIGNED:
-            return
-        res = super(SignupForm, self).do_signup(qcontext)
-        return res
+        supported_lang_codes = [code for code, _ in request.env['res.lang'].get_installed()]
+        lang = request.context.get('lang', '')
+        if lang in supported_lang_codes:
+            values['lang'] = lang
+        return values
